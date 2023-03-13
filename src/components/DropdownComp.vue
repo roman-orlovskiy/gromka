e07c29388c8360889838aa73d33475476a23efa5
Роман Орловский <r.orlovskiy@sports.ru>
2023-03-13 11:03:51 +0300

    Svg иконка

--- src/components/DropdownComp.vue
+++ src/components/DropdownComp.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="dropdown">
-    <button class="dropdown__toggle" @click="toggleDropdown">{{ selectedOption }}</button>
+    <button class="dropdown__toggle" @click="toggleDropdown">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+        <path fill="none" d="M0 0h24v24H0z"/>
+        <path d="M12 1.75c-5.514 0-10 4.486-10 10s4.486 10 10 10 10-4.486 10-10-4.486-10-10-10zm0 2c4.411 0 8 3.589 8 8s-3.589 8-8 8-8-3.589-8-8 3.589-8 8-8zm0 6.5c-.69 0-1.25.56-1.25 1.25v2.5c0 .69.56 1.25 1.25 1.25s1.25-.56 1.25-1.25v-2.5c0-.69-.56-1.25-1.25-1.25zm0 4.5a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5z"/>
+      </svg>
+      {{ selectedOption }}
+    </button>
     <ul v-show="isOpen" class="dropdown__list">
       <li
         v-for="(option, index) in options" :key="index"
